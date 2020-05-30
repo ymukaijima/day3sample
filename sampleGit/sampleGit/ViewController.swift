f0a4661c194d629681462ac12a511c01ebfcffa2
yuming0000 <qqe.mukaijima@gmail.com>
2020-05-30 14:34:35 +0900

    first commit

---
+++ sampleGit/sampleGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sampleGit
+//
+//  Created by yumi on 2020/05/30.
+//  Copyright © 2020 Yumi Takahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
